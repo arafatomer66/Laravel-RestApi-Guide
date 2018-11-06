c861c50c56b16fb61695547179c31387aac64349
arafat <arafatomer66@gmail.com>
2018-11-06 23:20:24 +0600

    Creating Models for restapi

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+    const AVAILABLE_PRODUCT = 'available';
+    protected $fillable = [
+        'name','description','quantity','status','image','seller_id',
+    ];
+    // we should declar with const for the values when they hve fix values
+
+    public function isAvailable(){
+        return $this->status == Product::AVAILABLE_PRODUCT; //return true
+    }
+
 }
