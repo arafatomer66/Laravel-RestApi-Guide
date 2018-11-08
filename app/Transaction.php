a1e6386f2bb4e95d0eb20b669dffd53171f614b2
arafatomer <arafatomer66@gmail.com>
2018-11-08 11:24:51 +0600

    Starting factory

--- app/Transaction.php
+++ app/Transaction.php
@@ -1,25 +1,20 @@
 <?php
-
 namespace App;
 use App\Product;
 use App\Buyer;
-
-
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
-{
-    protected $fillable =[
-        'quantity',
-        'buyer_id',
-        'product_id',
-    ];
-
-    public function buyer(){
-        return $this->belongsTo(Buyer::class);
+    {
+        protected $fillable =[
+                'quantity',
+                'buyer_id',
+                'product_id',
+        ];
+        public function buyer(){
+                return $this->belongsTo(Buyer::class);
+            }
+        public function product(){
+                return $this->belongsTo(Product::class);
+            }
     }
-
-    public function product(){
-        return $this->belongsTo(Product::class);
-    }
-}
