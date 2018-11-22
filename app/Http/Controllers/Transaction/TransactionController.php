974d18fe728c788ea738cbabb378faf32d7d6902
arafat <arafatomer66@gmail.com>
2018-11-22 20:17:15 +0600

    Transaction  Controller

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Transaction;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use App\Transaction;
 
 class TransactionController extends ApiController
 {
@@ -14,7 +15,8 @@ class TransactionController extends ApiController
      */
     public function index()
     {
-        //
+        $transaction = Transaction::all();
+        return $this->showAll($transaction);
     }
 
     /**
@@ -44,9 +46,9 @@ class TransactionController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Transaction $transaction)
     {
-        //
+        return $this->showOne($transaction);
     }
 
     /**
