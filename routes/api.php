736f635c89da1d957c872d95b7a448417e98a0ab
arafat <arafatomer66@gmail.com>
2018-11-25 23:19:20 +0600

    relation between category and buyers

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::resource('categories','Category\CategoryController', ['except' => ['creat
 Route::resource('categories.products','Category\CategoryProductController', ['only' => ['index']]);
 Route::resource('categories.sellers','Category\CategorySellerController', ['only' => ['index']]);
 Route::resource('categories.transactions','Category\CategoryTransactionController', ['only' => ['index']]);
-
+Route::resource('categories.buyers','Category\CategoryBuyerController', ['only' => ['index']]);
 
 Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
 
