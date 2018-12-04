de6586c036394f5b4f52349fe5c7c7ed49a6cb5b
Arafat <arafatomer66@gmail.com>
2018-12-04 12:40:45 +0600

    Implementation of mail cont ..

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 use App\Product ;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserCreated;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,13 +22,17 @@ class AppServiceProvider extends ServiceProvider
             {
                 Schema::defaultStringLength(191);
                 //event
-                Product::updated(function($product){
-                     if($product->quantity == 0 && $product->isAvailable() ){
-                          $product->status = Product::UNAVAILABLE_PRODUCT ;
-
-                          $product->save();
-                     }
+                User::created(function($user){
+                    Mail::to($user)->send(new UserCreated($user));
                 });
+
+                Product::updated(function($product){
+                    if($product->quantity == 0 && $product->isAvailable() ){
+                         $product->status = Product::UNAVAILABLE_PRODUCT ;
+
+                         $product->save();
+                    }
+               });
             }
 
     /**
