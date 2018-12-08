7905a84a186da16a09849613487214342598b40a
Arafat <arafatomer66@gmail.com>
2018-12-08 11:57:24 +0600

    retry helper for sending mails

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,16 @@ class AppServiceProvider extends ServiceProvider
                 Schema::defaultStringLength(191);
                 //event
                 User::created(function($user){
+                    retry(5 , function() use ($user){
                         Mail::to($user)->send(new UserCreated($user));
+                    },100);
+
                 });
                 User::updated(function($user){
                     if($user->isDirty('email')){
-                        Mail::to($user)->send(new UserMailChanged($user));
+                        retry(5 , function() use ($user){
+                            Mail::to($user)->send(new UserCreated($user));
+                        },100);
                     }
                 });
                 Product::updated(function($product){
