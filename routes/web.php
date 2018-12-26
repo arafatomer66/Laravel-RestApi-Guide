796df921263344f685e525e4e77c3856374cf62a
Arafat <arafatomer66@gmail.com>
2018-12-26 14:23:37 +0600

    node cont

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ $this->get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 
 Route::get('/', function(){
      return view('welcome');
